9d49b2626e51648da428c033d339645af6d9dac7
Jaime Caballero <me@jaicab.com>
2023-10-24 13:18:31 +0900

    Add footer

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./assets").addPassthroughCopy("./images")
+    eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
   
     return {
       passthroughFileCopy: true,
